a13a610555bef55a8cdcb71a62866f84e5055027
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2022-08-13 22:25:50 +0200

    add random number of orders to queue

--- lambdas/order-lambda.ts
+++ lambdas/order-lambda.ts
@@ -5,15 +5,22 @@ const orderQueueUrl = process.env.ORDER_QUEUE_URL;
 const sqs = new AWS.SQS();
 
 export async function handler() {
-  console.log("Order lambda called");
+  const numberOfOrders = Math.random() * 50;
+
+  console.log(`Order lambda called with ${numberOfOrders} orders`);
 
   if (!orderQueueUrl) {
     throw "Environment variable ORDER_QUEUE_URL not set";
   }
 
-  await sqs
-    .sendMessage({ QueueUrl: orderQueueUrl, MessageBody: "New order received" })
-    .promise();
+  for (let i = 0; i < numberOfOrders; i++) {
+    await sqs
+      .sendMessage({
+        QueueUrl: orderQueueUrl,
+        MessageBody: "New order received",
+      })
+      .promise();
 
-  console.log("Message sent to order queue");
+    console.log("Message sent to order queue");
+  }
 }
