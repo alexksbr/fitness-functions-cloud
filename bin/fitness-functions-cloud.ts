9bc9558734ddaf057299b38f3f6035004e6c00a0
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2022-08-15 11:48:50 +0200

    add ecs container with sample application

--- bin/fitness-functions-cloud.ts
+++ bin/fitness-functions-cloud.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
+import { ContainerStack } from "../lib/container-stack";
 import { QueueStack } from "../lib/queue-stack";
 
 const app = new cdk.App();
 new QueueStack(app, "QueueStack");
+new ContainerStack(app, "ContainerStack");
---
+++ lib/container-stack.ts
@@ -0,0 +1,31 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
+
+export class ContainerStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const cluster = new ecs.Cluster(this, "Cluster");
+
+    cluster.addCapacity("DefaultAutoScalingGroupCapacity", {
+      instanceType: new ec2.InstanceType("t2.small"),
+      desiredCapacity: 3,
+    });
+
+    const taskDefinition = new ecs.Ec2TaskDefinition(this, "TaskDef");
+
+    taskDefinition.addContainer("DefaultContainer", {
+      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      memoryLimitMiB: 512,
+      portMappings: [{ containerPort: 80 }],
+    });
+
+    new ecsPatterns.ApplicationLoadBalancedEc2Service(this, "Service", {
+      cluster,
+      taskDefinition,
+    });
+  }
+}
