b3e3a6df245c92b2fbd46e7b4c9733efe3419b72
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2022-08-16 22:03:00 +0100

    add cloudmap to ecs cluster

--- stacks/container-stack/container-stack.ts
+++ stacks/container-stack/container-stack.ts
@@ -2,12 +2,15 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as cloudmap from "aws-cdk-lib/aws-servicediscovery";
 
 export class ContainerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const cluster = new ecs.Cluster(this, "Cluster");
+    const cluster = new ecs.Cluster(this, "Cluster", {
+      defaultCloudMapNamespace: { name: "CloudMap" },
+    });
 
     cluster.addCapacity("DefaultAutoScalingGroupCapacity", {
       instanceType: new ec2.InstanceType("t2.small"),
@@ -32,6 +35,7 @@ export class ContainerStack extends Stack {
       cluster,
       taskDefinition,
       desiredCount: 1,
+      cloudMapOptions: { dnsRecordType: cloudmap.DnsRecordType.SRV },
     });
   }
 }
