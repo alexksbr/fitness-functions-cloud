5ed71849384993ad13006df4e5261493f64dec51
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2022-08-16 22:39:00 +0100

    add financial-service

---
+++ stacks/container-stack/services/financial-service/server.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require("express");
+
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+const app = express();
+app.get("/stocks", (req, res) => {
+  res.send([{ stockName: "XYZ INC", stockValue: 42 }]);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
